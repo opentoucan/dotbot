a0a037d80c8db5b60834b56235f5727c4988563f
jared <git@sillock.io>
2025-02-14 18:06:12 +0000

    chore: added devcontainer features for managing the workspace and service dependencies

---
+++ scripts/update-discord-interaction-endpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -s ngrok:4040/api/tunnels | \
+  jq .tunnels[].public_url | \
+  xargs -I '$' curl -X PATCH \
+    -d '{"interactions_endpoint_url": "$/interactions"}' \
+    -H "Authorization: Bot ${Discord__Token}" \
+    -H "Content-Type: application/json" \
+    https://discord.com/api/applications/@me
