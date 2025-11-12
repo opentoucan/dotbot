3aba75c7db1fcb01cefa053750ec4e7a646eb3d5
sillock <mail@sillock.com>
2025-11-12 15:30:15 +0000

    chore(renovate): moar renovate

--- .renovate/post-upgrade-dotnet-tfm.sh
+++ .renovate/post-upgrade-dotnet-tfm.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 NEW_SDK_VERSION=$(grep '"version":' ./global.json | cut -d '"' -f 4 | grep -Po '^[0-9]+')
 sed -r -i -e 's|<TargetFramework>(net[0-9\.]+)</TargetFramework>|<TargetFramework>net'"${NEW_SDK_VERSION}"'.0</TargetFramework>|g' ./Directory.Packages.props
